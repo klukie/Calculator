e618a2019b0587a4dd9924c89c95c06a542ea316
klukie <klukie1@yahoo.com>
2021-02-19 00:46:17 -0800

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Ryan Kearney on 2/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
